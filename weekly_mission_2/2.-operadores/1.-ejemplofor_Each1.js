adc53dad916ae436eed67f7941b62d9f8ea0f249
eduardocanoperez <eduardo_cano_perez@live.com.mx>
2022-04-19 19:51:32 -0500

    update excersise2

---
+++ weekly_mission_2/2.-operadores/1.-ejemplofor_Each1.js
@@ -0,0 +1,3 @@
+const numbers = [1, 2, 3, 4, 5]
+console.log("ejemplo 1: imprimiendo los elementos de una lista")
+numbers.forEach(num => console.log(num))
---
+++ weekly_mission_2/2.-operadores/2.-ejemplofor_Each2.js
@@ -0,0 +1,5 @@
+let sum = 0
+const numbers2 = [1, 2, 3, 4, 5]
+numbers2.forEach(num => sum += num)
+console.log("ejemplo 2: calculo de la suma de los elementos de la lista")
+console.log(sum)
