036351ff6ea497310fd40344fae602e286c05420
eduardocanoperez <eduardo_cano_perez@live.com.mx>
2022-05-09 11:12:16 -0500

    agregando nueva ruta (launchx)

--- weekly_mission_3/expres_basic/app.js
+++ weekly_mission_3/expres_basic/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const req = require('express/lib/request')
 const app = express()
 const port = 3000 //localhost:3000
 
 //localhost:3000
 app.get('/', (req, res) => {
     res.send("hola a todos mis explorers")
+})
+
+//localhost:3000
+app.get('/launchx', (req, res) => {
+    res.send("bienvenidos a launch X")
 })
 
 app.listen(port, () => {
