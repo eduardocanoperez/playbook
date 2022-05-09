0398b8cc6f0b056895ff13ce4e98de11cc1d7b21
eduardocanoperez <eduardo_cano_perez@live.com.mx>
2022-05-09 11:24:05 -0500

    regresando una lista de objetos con express

--- weekly_mission_3/expres_basic/app.js
+++ weekly_mission_3/expres_basic/app.js
@@ -13,6 +13,14 @@ app.get('/launchx', (req, res) => {
     res.send("bienvenidos a launch X")
 })
 
+app.get('/node', (req, res) => {
+    const explorer1 = { id: 1, name: "explorer"}
+    const explorer2 = { id: 2, name: "explorer"}
+    const explorer3 = { id: 3, name: "explorer"}
+
+    res.send([explorer1, explorer2, explorer3])
+})
+
 app.listen(port, () => {
     console.log("server listo!!!")
 })
